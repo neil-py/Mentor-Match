8b7308860fac33e5f751af254227291280de0c5e
Neil Patrick Villanueva <neilpatz16@gmail.com>
2024-01-30 21:28:00 +0800

    updated gitignore

---
+++ app/models/tutor_applications.py
@@ -0,0 +1,8 @@
+from app.extensions import db
+
+class TutorApplications(db.Model):
+    __tablename__ = "TUTOR_APPLICATIONS"
+    id = db.Column(db.Integer, primary_key=True)
+    student_id = db.Column(db.Integer, db.ForeignKey('USERS.id'))  # Use 'users.id' since 'users' is the tablename of Users model
+    student = db.relationship('Users', foreign_keys=[student_id], backref='student_sessions')
+    
